dffbc24721ccc887f0141352e00c771a2b2b86f2
Zoe Ames <zoeames@gmail.com>
2014-08-20 09:34:29 -0500

    initial files

---
+++ app/routes/routes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var morgan         = require('morgan'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('express-method-override'),
+    less           = require('less-middleware'),
+    home           = require('../controllers/home');
+
+module.exports = function(app, express){
+  app.use(morgan('dev'));
+  app.use(less(__dirname + '/../static'));
+  app.use(express.static(__dirname + '/../static'));
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride());
+
+  app.get('/', home.index);
+
+  console.log('Express: Routes Loaded');
+};
+
